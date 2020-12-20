543230d38e6abdc29f47427b506af1a974e1f738
hongtao <realhanton@gmail.com>
2020-12-20 19:13:52 +0000

    v6 ex6 submit 2

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,14 +97,13 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
+    vocabList_length = length(vocabList);
+    for idx = 1 : vocabList_length
+      if (strcmp(str, vocabList{idx}) == 1)
+        word_indices = [word_indices ; idx];
+        break;
+      end
+    end
 
     % =============================================================
 
